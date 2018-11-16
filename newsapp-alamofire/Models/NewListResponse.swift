f46ee077e7a72d31cdd68ee8fc185f530afddd56
Martino Christanto Khuangga <13514084@std.stei.itb.ac.id>
2018-11-16 08:27:55 +0700

    Create mapping for API response

---
+++ newsapp-alamofire/Models/NewListResponse.swift
@@ -0,0 +1,28 @@
+//
+//  NewListResponse.swift
+//  newsapp-alamofire
+//
+//  Created by Nakama on 16/11/18.
+//  Copyright © 2018 ridhopratama. All rights reserved.
+//
+
+import Foundation
+
+struct ArticleSource: Decodable {
+    let id: String?
+    let name: String
+}
+
+struct Article: Decodable {
+    let source: ArticleSource
+    let author: String
+    let title: String
+    let description: String
+    let urlToImage: String
+}
+
+struct ArticleResponse: Decodable {
+    let articles: [Article]
+    let status: String
+    let totalResults: Int
+}
